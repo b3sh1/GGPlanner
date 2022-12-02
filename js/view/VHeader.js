d2d71fc5d4b2cdebe153ac873a48f4b2081d5717
besi <besi3970@gmail.com>
2022-12-02 02:34:13 +0100

    added few tooltips

--- js/view/VHeader.js
+++ js/view/VHeader.js
@@ -13,16 +13,16 @@ let html = `
         <!--Grid column-->
         <div class="col">
             <div class="text-info" style="font-size: 1.5em">
-                <span>
-                    <a id="btn-export" class="link-info"  href="#!"" data-mdb-toggle="modal"
-                                    data-mdb-target="#modal-export">
+                <span title="Save current data to file.">
+                    <a id="btn-export" class="link-info"  href="#!" data-mdb-toggle="modal"
+                            data-mdb-target="#modal-export">
                         <i class="fas fa-save"></i>
                     </a>
 <!--                    <button id="btn-export" type="button" class="btn btn-link btn-lg ripple-surface ripple-surface-dark" data-mdb-ripple-color="dark" style="font-size: 1em">-->
 <!--                        <i class="fas fa-save"></i>-->
 <!--                    </button>-->
                 </span>
-                <span>
+                <span title="Load data from file.">
                     <a id="btn-import" class="link-info" href="#!">
                         <i class="fas fa-file-import"></i>
                     </a>
@@ -30,9 +30,9 @@ let html = `
 <!--                        <i class="fas fa-file-import"></i>-->
 <!--                    </button>-->
                 </span>
-                <span>
+                <span title="Delete all current data.">
                     <a id="btn-destroy" class="link-info" href="#!" data-mdb-toggle="modal"
-                                    data-mdb-target="#modal-destroy-data">
+                            data-mdb-target="#modal-destroy-data">
                         <i class="fas fa-trash-alt"></i>
                     </a>
 <!--                    <button id="btn-destroy" type="button" class="btn btn-link btn-lg ripple-surface ripple-surface-dark" data-mdb-ripple-color="dark" style="font-size: 1em">-->
