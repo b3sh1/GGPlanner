fa68f6d1dbcbec48b5959ea7d3ebbd93e7b37f67
besi <besi3970@gmail.com>
2022-12-02 22:13:45 +0100

    footer

--- js/view/VFooter.js
+++ js/view/VFooter.js
@@ -6,14 +6,14 @@ let html = `
     <!-- Grid container -->
 
     <div class="text-center">
-        Special thanks to:
-        <a href="https://www.hattrick.org/Club/Manager/?userId=5176908" target="_blank">Schum</a>
-    </div>
-    <div class="text-center">
         References:
         <a href="https://www.hattrick.org/Forum/Read.aspx?n=1&t=17445814" target="_blank">Unwritten manual</a>,
         <a href="https://github.com/akasolace/HO/" target="_blank">Hattrick Organizer</a>,
         <a href="https://github.com/tagliala/WageWizard/" target="_blank">WageWizard</a>
+    </div>
+    <div class="text-center">
+        Special thanks to:
+        <a href="https://www.hattrick.org/Club/Manager/?userId=5176908" target="_blank">Schum</a>
     </div>
     <!-- Copyright -->
     <div class="text-center p-3">
