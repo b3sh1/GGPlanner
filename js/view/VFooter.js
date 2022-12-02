706033f064760dd40687af6f8720b39ece81c8b2
besi <besi3970@gmail.com>
2022-12-02 11:32:56 +0100

    footer with references

--- js/view/VFooter.js
+++ js/view/VFooter.js
@@ -10,7 +10,7 @@ let html = `
         <a href="https://www.hattrick.org/Club/Manager/?userId=5176908">Schum</a>
     </div>
     <div class="text-center">
-        Formula Sources:
+        References:
         <a href="https://www.hattrick.org/Forum/Read.aspx?n=1&t=17445814">Unwritten manual</a>,
         <a href="https://github.com/akasolace/HO/">Hattrick Organizer</a>,
         <a href="https://github.com/tagliala/WageWizard/">WageWizard</a>,
