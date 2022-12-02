2fd5edfaed9ab73ab2287e21eae2d09a8dc34987
besi <besi3970@gmail.com>
2022-12-02 15:21:22 +0100

    footer - open links in new window/tab

--- js/view/VFooter.js
+++ js/view/VFooter.js
@@ -7,18 +7,18 @@ let html = `
 
     <div class="text-center">
         Special thanks to:
-        <a href="https://www.hattrick.org/Club/Manager/?userId=5176908">Schum</a>
+        <a href="https://www.hattrick.org/Club/Manager/?userId=5176908" target="_blank">Schum</a>
     </div>
     <div class="text-center">
         References:
-        <a href="https://www.hattrick.org/Forum/Read.aspx?n=1&t=17445814">Unwritten manual</a>,
-        <a href="https://github.com/akasolace/HO/">Hattrick Organizer</a>,
-        <a href="https://github.com/tagliala/WageWizard/">WageWizard</a>
+        <a href="https://www.hattrick.org/Forum/Read.aspx?n=1&t=17445814" target="_blank">Unwritten manual</a>,
+        <a href="https://github.com/akasolace/HO/" target="_blank">Hattrick Organizer</a>,
+        <a href="https://github.com/tagliala/WageWizard/" target="_blank">WageWizard</a>
     </div>
     <!-- Copyright -->
     <div class="text-center p-3">
         © 2022:
-        <a href="https://www.hattrick.org/en-us/Club/Manager/?teamId=1120189">beshi</a>
+        <a href="https://www.hattrick.org/en-us/Club/Manager/?teamId=1120189" target="_blank">beshi</a>
     </div>
     <!-- Copyright -->
 </footer>
