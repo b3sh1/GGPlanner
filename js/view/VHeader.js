dacd3585b61d3ed22979bb6e57e2a519192a634c
besi <besi3970@gmail.com>
2022-12-03 12:17:59 +0100

    about modal

--- js/view/VHeader.js
+++ js/view/VHeader.js
@@ -13,7 +13,7 @@ let html = `
         <!--Grid column-->
         <div class="col">
             <div class="text-info" style="font-size: 1.5em">
-                <span title="Save current data to file.">
+                <span title="Save current data to file">
                     <a id="btn-export" href="#!" data-mdb-toggle="modal"
                             data-mdb-target="#modal-export">
                         <i class="fas fa-save"></i>
@@ -22,7 +22,7 @@ let html = `
 <!--                        <i class="fas fa-save"></i>-->
 <!--                    </button>-->
                 </span>
-                <span title="Load data from file.">
+                <span title="Load data from file">
                     <a id="btn-import" href="#!">
                         <i class="fas fa-file-import"></i>
                     </a>
@@ -30,7 +30,7 @@ let html = `
 <!--                        <i class="fas fa-file-import"></i>-->
 <!--                    </button>-->
                 </span>
-                <span title="Delete all current data.">
+                <span title="Delete all current data">
                     <a id="btn-destroy" href="#!" data-mdb-toggle="modal"
                             data-mdb-target="#modal-destroy-data">
                         <i class="fas fa-trash-alt"></i>
@@ -38,6 +38,12 @@ let html = `
 <!--                    <button id="btn-destroy" type="button" class="btn btn-link btn-lg ripple-surface ripple-surface-dark" data-mdb-ripple-color="dark" style="font-size: 1em">-->
 <!--                        <i class="fas fa-trash-alt"></i>-->
 <!--                    </button>-->
+                </span>
+                <span title="About this website">
+                    <a id="btn-about" href="#!" data-mdb-toggle="modal"
+                            data-mdb-target="#modal-about">
+                        <i class="fas fa-question"></i>
+                    </a>
                 </span>
             </div>
         </div>
